6b235148a3f6d1b6ba23c37660efeb2d556d4b35
Stoyan Yordanov <137334856+forklift-operator@users.noreply.github.com>
2025-12-23 23:49:37 +0200

    Small refactoring

--- src/bg/sofia/uni/fmi/mjt/steganography/worker/EmbedConsumer.java
+++ src/bg/sofia/uni/fmi/mjt/steganography/worker/EmbedConsumer.java
@@ -12,29 +12,20 @@ import java.util.concurrent.BlockingQueue;
 
 public class EmbedConsumer implements Runnable {
     private final BlockingQueue<EmbedTask> imageTasks;
-    private final SteganographyAlgorithm embedAlgorithm;
+    private final SteganographyAlgorithm algorithm;
 
 
     public EmbedConsumer(BlockingQueue<EmbedTask> imageTasks, SteganographyAlgorithm embedAlgorithm) {
         if (imageTasks == null) {
-            throw new IllegalArgumentException("Image tasks cannot be null");
+            throw new IllegalArgumentException("Image tasks queue cannot be null");
         }
 
         if (embedAlgorithm == null) {
             throw new IllegalArgumentException("Embedding algorithm cannot be null");
         }
 
-        this.embedAlgorithm = embedAlgorithm;
+        this.algorithm = embedAlgorithm;
         this.imageTasks = imageTasks;
-    }
-
-
-    public void saveImage(BufferedImage image, String outputDir, String name) {
-        try {
-            ImageIO.write(image, "png", new File(outputDir, name));
-        } catch (IOException e) {
-            throw new UncheckedIOException(String.format("While saving image %s", name), e);
-        }
     }
 
     @Override
@@ -55,7 +46,15 @@ public class EmbedConsumer implements Runnable {
     }
 
     void consume(EmbedTask task) {
-        BufferedImage embedded = embedAlgorithm.embed(task.cover(), task.secret());
+        BufferedImage embedded = algorithm.embed(task.cover(), task.secret());
         saveImage(embedded, task.outputPath(), task.coverName() + ".png");
+    }
+
+    private void saveImage(BufferedImage image, String outputDir, String name) {
+        try {
+            ImageIO.write(image, "png", new File(outputDir, name));
+        } catch (IOException e) {
+            throw new UncheckedIOException(String.format("While saving image %s", name), e);
+        }
     }
 }
